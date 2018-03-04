7e92c249807404fd49f29d7bf4217371339f9c81
Tiffany Zheng <qz3@williams.edu>
2018-03-04 10:09:55 -0500

    incorporated file stuff w google

--- AudioReader.py
+++ AudioReader.py
@@ -21,48 +21,54 @@ transcrpt = 'Williams College'
 @app.route('/', methods=['GET','POST'])
 
 def AudioReader():
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"/Users/Xiao/Desktop/appstuff/CampusEar/CampusEar-2b476fe18f5a.json"
-# # Instantiates a client
-    client = speech.SpeechClient()
-# The name of the audio file to transcribe
-    file_name = os.path.join(
-        os.path.dirname(__file__),
-        'resources',
-        'testAudio.flac')#change this later
-# Loads the audio into memory
-    with io.open(file_name, 'rb') as audio_file:
-        content = audio_file.read()
-        audio = types.RecognitionAudio(content=content)
-
-    config = types.RecognitionConfig(
-            encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
-            sample_rate_hertz=48000,
-            language_code='en-US')#
-# Detects speech in the audio file
-    response = client.recognize(config, audio)
-    print("ended")
-    for result in response.results:
-        print('Transcript: {}'.format(result.alternatives[0].transcript))
 
     usr_loc = request.form.get('Location')
     usr_eml = request.form.get('Email')
     num_mins = request.form.get('NumMinutes')
     phrases = request.form.get('Phrases')
 
-    print(usr_loc)
-    print(usr_eml)
-    print(num_mins)
-    print(phrases)
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = r"/Users/Xiao/Desktop/appstuff/CampusEar/CampusEar-2b476fe18f5a.json"
 
+# # Instantiates a client
+    client = speech.SpeechClient()
+# The name of the audio file to transcribe
+    for file_name in os.listdir("/Users/Xiao/Desktop/appstuff/CampusEar/resources/output"):
+        
+        #file_name = os.path.join(
+            #os.path.dirname(__file__),
+            #'resources',
+            #'output'
+            #audio)#change this later
+# Loads the audio into memory
+        with io.open(file_name, 'rb') as audio_file:
+            content = audio_file.read()
+            audio = types.RecognitionAudio(content=content)
+            
+        config = types.RecognitionConfig(
+                encoding=enums.RecognitionConfig.AudioEncoding.FLAC,
+                sample_rate_hertz=48000,
+                language_code='en-US')#
+    # Detects speech in the audio file
+        response = client.recognize(config, audio)
+        print("ended")
+        for result in response.results:
+            transcrpt = ('Transcript: {}'.format(result.alternatives[0].transcript))
+
+            
+            # print(usr_loc)
+    #print(usr_eml)
+    #print(num_mins)
+    #print(phrases)
+            
 #do one phrase for now, we can figure out how to do multiple phrases later
-    if (phrases != None) and (phrases in transcrpt):
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login("campusear@gmail.com", "campusearpassword")
-
-        msg = transcrpt
-        server.sendmail("campusear@gmail.com", "qz3@williams.edu", msg)
-        server.quit()
+        if (phrases != None) and (phrases in transcrpt):
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login("campusear@gmail.com", "campusearpassword")
+            
+            msg = transcrpt
+            server.sendmail("campusear@gmail.com", "qz3@williams.edu", msg)
+            server.quit()
 
     return render_template("index.html")
 
